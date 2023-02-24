2a43c4d777bd8378fa1bec130ec7c5a5d1f1a476
Titouan SALOU <titouan.salou.74@gmail.com>
2023-02-24 01:45:39 +0100

    need to add await on db select query to make it work

---
+++ .history/public/functions_20230224005843.js
@@ -0,0 +1,56 @@
+
+function assignActive(id) {
+    if(id){document.getElementById(id).classList.add("active")}
+}
+
+function printMessage(sender, message, date) {
+    let msg = document.createElement("li")
+    msg.innerHTML = `${sender}: ${message}`
+    msg.classList.add("list-group-item", "flexli")
+    document.querySelector('#messages').appendChild(msg);
+    let time = document.createElement("span")
+    time.innerHTML = date
+    msg.appendChild(time);
+}
+
+function initDB() {
+    const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+     host: 'localhost', 
+     user:'root', 
+     password: '',
+     database:"beerChat"
+});
+return pool
+}
+
+function insertMessage(pool, content, datetime, sender){
+    pool.getConnection()
+    .then(conn => {
+    
+    conn.query(`INSERT INTO message(content, datetime, sender) value('${content}', '${datetime}', '${sender}')`)
+        .then((rows) => {
+          console.log(rows);
+          return ;
+        })
+        .then((res) => {
+          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          conn.end();
+        })
+        .catch(err => {
+          //handle error
+          console.log(err); 
+          conn.end();
+        })
+        
+    }).catch(err => {
+      //not connected
+    });
+}
+
+module.exports = {
+    assignActive: assignActive,
+    printMessage: printMessage,
+    initDB: initDB,
+    insertMessage: insertMessage
+  };
---
+++ .history/server_20230224012250.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const axios = require('axios')
+const { Server } = require('socket.io');
+const http = require('http')
+const path = require('path');
+const { render } = require('pug');
+//socket server
+const app = express()
+const server = http.createServer(app)
+const myfunctions = require('./public/functions')
+console.log(myfunctions)
+dotenv.config()
+app.set("view engine", "pug");
+app.set('views', './views')
+
+app.use(express.static('public'))
+app.use(express.static('node_modules'))
+
+
+app.get('/', (req, res) => {
+    res.redirect(301, `http://localhost:${process.env.port}/accueil`)
+})
+
+app.get('/accueil', (req, res) => {
+    renderHtml(res, 'accueil', 'Punk: Home')
+})
+
+app.get('/boissons', async (req, res) => {
+    let resp = await axios('https://api.punkapi.com/v2/beers')
+    renderHtml(res, 'boissons', 'Punk: Beers', resp.data)
+})
+
+app.get('/boisson-details/:id', async (req, res) => {
+    let beerId = req.params.id
+    let beer = await axios(`https://api.punkapi.com/v2/beers/${beerId}`)
+    renderHtml(res, 'boisson_details', beer.data[0].name, beer.data[0])
+
+})
+
+app.get('/random', async (req, res) => {
+    let beer = await axios(`https://api.punkapi.com/v2/beers/random`)
+    renderHtml(res, 'random_boisson', beer.data[0].name, beer.data[0])
+})
+
+//renvoie la page contact
+app.get('/contact', (req, res) => {
+    renderHtml(res, "contact", "Contact us")
+})
+
+// autorise les connections uniquement depuis localhost:8080, NE MARCHE PAS SANS
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Ecoute les connections au socket
+io.on("connection", (socket) => {
+
+    socket.on("message", obj => {
+        let registerDb = false
+        if (obj.message.slice(0, 10) === '/storeToDb') {
+            registerDb = true
+        }
+        if(!!registerDb){
+            myfunctions.insertMessage(myfunctions.initDB(), obj.message, obj.date, obj.sender)
+        }
+        // broadcast le message
+        io.emit('message', {content: obj.message, datetime: obj.date, sender:obj.sender})
+        // envoi un message automatique
+        io.emit('response', {content: 'We received your message and will get back to you ASAP !', sender: 'PunkBot', datetime:obj.date})
+    });
+});
+
+
+server.listen(3000, () => {
+    console.log('socket listening on port 3000')
+})
+
+app.listen(process.env.port, () => {
+    console.log(`webserver listening on port ${process.env.port}`)
+})
+
+
+function renderHtml(res, page, title, data = null) {
+    res.header('Content-Type', 'text/html; charset=utf-8');
+    //la page détails n'a pas de lien dans la navbar, j'annule donc le paramètre active
+    if (page === 'boisson_details') {
+        res.render(page, { title: title, data: data })
+        return
+    }
+    res.render(page, { title: title, active: page, data: data })
+}
